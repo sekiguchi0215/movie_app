5ee4df6ddbca4d04cb8712289033f81e5a605d8c
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-28 22:23:36 +0900

    動画保存時にフォーマットされるよう設定

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,14 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
   validates :url, presence: true
+
+  before_save do
+    format_url = YoutubeUrlFormatter.format(url)
+    if format_url.present?
+      self.url = format_url
+    else
+      self.errors.add(:url, "YoutubeのURL以外は無効です。")
+      throw(:abort)
+    end
+  end
 end
